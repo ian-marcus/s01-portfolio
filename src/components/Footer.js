55ae69ce9dfbe48c2b0648430016f8ecf6904f15
ian-marcus <ibeasianmarcus@gmail.com>
2021-09-21 20:56:09 +0800

    edit the navbar links and background color

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer () {
+	return (
+
+		<div>
+			Copyright © Portfolio 2021. All rights reserved 
+		</div>
+
+	)
+}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,12 +5,12 @@ import Nav from 'react-bootstrap/Nav'
 
 export default function AppNavbar() {
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar bg="dark" variant="dark" expand="lg">
         <Navbar.Brand href="#home">Sample Project</Navbar.Brand>
             <Nav className="mr-auto">
                 <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
-                <Nav.Link href="#link">About</Nav.Link>
+                <Nav.Link href="https://zuitt.co/" target="_blank">Zuitt</Nav.Link>
+                <Nav.Link href="https://www.facebook.com/zuittcodingbootcamp/" target="_blank">Facebook Page</Nav.Link>
             </Nav>
         </Navbar>
     )
